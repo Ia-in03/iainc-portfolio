d3714e850721dc57f63b4258e9b55b7de7ddfe48
Iain Cline <iaincline@outlook.com>
2026-05-23 00:14:27 -0500

    Placeholder them pages

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
+      <h1 className="text-3xl font-semibold mb-6">About</h1>
+      <p className="text-lg">Some stuff about me goes here</p>
+    </main>
+  );
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
+      <h1 className="text-3xl font-semibold mb-6">Projects</h1>
+      <p className="text-lg">Some of my projects go here</p>
+    </main>
+  );
+}
